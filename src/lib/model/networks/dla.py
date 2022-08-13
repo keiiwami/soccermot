16d18ba41efa11c9e0fd195d8df64a59a4c42489
keiiwami <19fi551@ms.dendai.ac.jp>
2022-08-14 00:00:57 +0900

    feat: demoの追加

--- src/lib/model/networks/dla.py
+++ src/lib/model/networks/dla.py
@@ -510,8 +510,6 @@ class DeformConv(nn.Module):
             nn.BatchNorm2d(cho, momentum=BN_MOMENTUM),
             nn.ReLU(inplace=True)
         )
-        print(chi)
-        print(cho)
         self.conv = DCN(chi, cho, kernel_size=(3,3), stride=1, padding=1, dilation=1, deformable_groups=1)
 
     def forward(self, x):
