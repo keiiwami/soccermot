439ac92fe6c3e810e46247d17482c968cf91e389
keiiwami <19fi551@ms.dendai.ac.jp>
2022-08-14 00:44:30 +0900

    fix: cv2のエンコード変更

--- src/demo.py
+++ src/demo.py
@@ -46,8 +46,8 @@ def demo(opt):
   out_name = opt.demo[opt.demo.rfind('/') + 1:]
   print('out_name', out_name)
   if opt.save_video:
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    # fourcc = cv2.VideoWriter_fourcc(*'H264')
+    # fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    fourcc = cv2.VideoWriter_fourcc(*'H264')
     out = cv2.VideoWriter('../results/{}.mp4'.format(
       opt.exp_id + '_' + out_name),fourcc, opt.save_framerate, (
         opt.video_w, opt.video_h))
